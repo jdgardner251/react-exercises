1fef30e647cab94d0b8598b13c9e19abafc68925
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-09-15 15:37:49 -0500

    added arrows

--- src/Stats.js
+++ src/Stats.js
@@ -7,18 +7,21 @@ function Stats(props) {
     }
 
     const percentChange = parseInt(props.change)
+    let percentChangeRendered = ""
     
     if (percentChange >= 0){
         customStyle.backgroundColor = "green"
+        percentChangeRendered =  `↖ ${percentChange}`
     } else {
         customStyle.backgroundColor = "red"
+        percentChangeRendered =  `↘ ${percentChange}`
     }
 
     return (
         <div className="stat-card">
             <h3 className="group">{props.label}</h3>
             <p className="item">{props.value}</p>
-            <p className="item" style={customStyle}>{percentChange}%</p>
+            <p className="item" style={customStyle}>{percentChangeRendered}%</p>
         </div>
     )
 }
